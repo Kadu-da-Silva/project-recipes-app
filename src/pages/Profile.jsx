339e8417c7409f515014c894728ff1a965c67aa8
Alexandre <alexandretigalanti@gmail.com>
2023-06-05 23:43:56 -0300

    requisitos 57,58,59 e 61

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../Components/Header';
 
 function Profile() {
+  const [emailProfile, setEmailProfile] = useState('');
+  const history = useHistory();
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem('user');
+    if (storedUser !== null) {
+      setEmailProfile(storedUser);
+    }
+  }, []);
+
+  const doneRecipesClick = () => {
+    history.push('/done-recipes');
+  };
+
+  const favoriteClick = () => {
+    history.push('/favorite-recipes');
+  };
+
   return (
     <div>
       <Header search={ false } name="Profile" />
+      <h3 data-testid="profile-email">{emailProfile}</h3>
+      <div>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ doneRecipesClick }
+        >
+          Done Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ favoriteClick }
+        >
+          Favorite Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ () => {
+            localStorage.clear();
+            history.push('/');
+          } }
+        >
+          Logout
+        </button>
+      </div>
     </div>
   );
 }
