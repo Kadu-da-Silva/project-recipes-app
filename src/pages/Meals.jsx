2bd22933b85c9194365ca17878cfd9d7e9290b58
Francisco Motta <motta.lopes@hotmail.com>
2023-06-06 11:32:52 -0300

    footer and tests ok

--- src/pages/Meals.jsx
+++ src/pages/Meals.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../Components/Header';
+import Footer from '../Components/Footer';
 
 function Meals() {
   return (
     <div>
       <Header search name="Meals" />
+      <Footer />
     </div>
   );
 }
