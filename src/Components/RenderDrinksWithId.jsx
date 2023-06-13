e6b20b3bbdca409b3af6d34a982c5151c5be460c
Carlos Eduardo <carlao0010@hotmail.com>
2023-06-13 15:25:30 -0300

    req 30 OK

---
+++ src/Components/RenderDrinksWithId.jsx
@@ -0,0 +1,151 @@
+import PropTypes from 'prop-types';
+import React, { useState, useEffect } from 'react';
+import style from '../pages/RecipeDetails.module.css';
+
+function RenderDrinksWithId({ drinks, mealsRecommendation, loading }) {
+  const [isRecipeInProgress, setIsRecipeInProgress] = useState(false);
+
+  useEffect(() => {
+    // Verificar se a receita está salva no localStorage
+    const inProgressRecipesJSON = localStorage.getItem('inProgressRecipes');
+    if (inProgressRecipesJSON) {
+      const inProgressRecipes = JSON.parse(inProgressRecipesJSON);
+      if (inProgressRecipes.drinks && inProgressRecipes.drinks[drinks[0].idDrink]) {
+        setIsRecipeInProgress(true);
+      }
+    }
+  }, [drinks]);
+
+  const drink = drinks[0];
+
+  const renderIngredientsDrinks = () => {
+    const ingredients = [];
+    const MAX_LENGTH = 15;
+
+    for (let i = 0; i <= MAX_LENGTH; i += 1) {
+      const ingredient = drink[`strIngredient${i}`];
+      const measure = drink[`strMeasure${i}`];
+
+      if (ingredient && measure) {
+        ingredients.push(
+          <li
+            key={ `ingredient-${i}` }
+            data-testid={ `${i - 1}-ingredient-name-and-measure` }
+          >
+            {`${measure} - ${ingredient}`}
+          </li>,
+        );
+      }
+    }
+
+    return ingredients;
+  };
+
+  const renderMealsRecommendation = () => {
+    if (loading) {
+      return <p>Loading recommendation...</p>;
+    }
+
+    return (
+      <div className={ style.carousel }>
+        <div className={ style.carousel_inner }>
+          {mealsRecommendation.map(({ strMeal, strMealThumb }, index) => (
+            <div
+              key={ index }
+              className={ style.carousel_item }
+              data-testid={ `${index}-recommendation-card` }
+            >
+              <img src={ strMealThumb } alt={ strMeal } />
+              <p data-testid={ `${index}-recommendation-title` }>{strMeal}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  };
+
+  const handleClick = () => {
+    const { idDrink } = drink;
+    const ingredients = [];
+    const MAX_LENGTH = 15;
+
+    for (let i = 0; i <= MAX_LENGTH; i += 1) {
+      const ingredient = drink[`strIngredient${i}`];
+      const measure = drink[`strMeasure${i}`];
+
+      if (ingredient && measure) {
+        ingredients.push(
+          `${measure} - ${ingredient}`,
+        );
+      }
+    }
+
+    // Passo 1: Recuperar o valor atual do localStorage
+    const inProgressRecipesJSON = localStorage.getItem('inProgressRecipes');
+    let inProgressRecipes = {};
+
+    // Verificar se já existe um valor no localStorage
+    if (inProgressRecipesJSON) {
+      inProgressRecipes = JSON.parse(inProgressRecipesJSON);
+    }
+
+    // Passo 2: Adicionar ou atualizar os dados desejados na estrutura do objeto
+    if (!inProgressRecipes.drinks) {
+      inProgressRecipes.drinks = {};
+    }
+
+    inProgressRecipes.drinks[idDrink] = ingredients;
+
+    // Passo 3: Salvar o objeto atualizado de volta no localStorage
+    localStorage.setItem('inProgressRecipes', JSON.stringify(inProgressRecipes));
+  };
+
+  if (!drinks) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <section className={ style.section }>
+      <h1 data-testid="recipe-title">{drink.strDrink}</h1>
+      <div className={ style.containerImg }>
+        <img
+          src={ drink.strDrinkThumb }
+          alt={ drink.strDrink }
+          data-testid="recipe-photo"
+          className={ style.imgRecipe }
+        />
+      </div>
+      <p
+        data-testid="recipe-category"
+        className={ style.category }
+      >
+        {drink.strAlcoholic}
+      </p>
+      <h2>Ingredients:</h2>
+      <div className={ style.container }>
+        <ul>{renderIngredientsDrinks()}</ul>
+      </div>
+      <h2>Instructions:</h2>
+      <div className={ style.container }>
+        <p data-testid="instructions">{drink.strInstructions}</p>
+      </div>
+      <h2>Recommendation</h2>
+      {renderMealsRecommendation()}
+      <button
+        data-testid="start-recipe-btn"
+        className={ style.btnStart }
+        onClick={ handleClick }
+      >
+        {isRecipeInProgress ? 'Continue Recipe' : 'Start Recipe'}
+      </button>
+    </section>
+  );
+}
+
+RenderDrinksWithId.propTypes = {
+  drinks: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+  loading: PropTypes.bool.isRequired,
+  mealsRecommendation: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+};
+
+export default RenderDrinksWithId;
---
+++ src/Components/RenderMealsWithId.jsx
@@ -0,0 +1,155 @@
+import PropTypes from 'prop-types';
+import React, { useState, useEffect } from 'react';
+import style from '../pages/RecipeDetails.module.css';
+
+function RenderMealsWithId({ meals, drinksRecommendation, loading }) {
+  const [isRecipeInProgress, setIsRecipeInProgress] = useState(false);
+
+  useEffect(() => {
+    // Verificar se a receita está salva no localStorage
+    const inProgressRecipesJSON = localStorage.getItem('inProgressRecipes');
+    if (inProgressRecipesJSON) {
+      const inProgressRecipes = JSON.parse(inProgressRecipesJSON);
+      if (inProgressRecipes.meals && inProgressRecipes.meals[meals[0].idMeal]) {
+        setIsRecipeInProgress(true);
+      }
+    }
+  }, [meals]);
+
+  const meal = meals[0];
+
+  const renderIngredientsMeals = () => {
+    const ingredients = [];
+    const MAX_LENGTH = 20;
+
+    for (let i = 0; i <= MAX_LENGTH; i += 1) {
+      const ingredient = meal[`strIngredient${i}`];
+      const measure = meal[`strMeasure${i}`];
+
+      if (ingredient && measure) {
+        ingredients.push(
+          <li
+            key={ `ingredient-${i}` }
+            data-testid={ `${i - 1}-ingredient-name-and-measure` }
+          >
+            {`${measure} - ${ingredient}`}
+          </li>,
+        );
+      }
+    }
+
+    return ingredients;
+  };
+
+  const renderDrinksRecommendation = () => {
+    if (loading) {
+      return <p>Loading recommendation...</p>;
+    }
+
+    return (
+      <div className={ style.carousel }>
+        <div className={ style.carousel_inner }>
+          {drinksRecommendation.map(({ strDrink, strDrinkThumb }, index) => (
+            <div
+              key={ index }
+              className={ style.carousel_item }
+              data-testid={ `${index}-recommendation-card` }
+            >
+              <img src={ strDrinkThumb } alt={ strDrink } />
+              <p data-testid={ `${index}-recommendation-title` }>{strDrink}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  };
+
+  const handleClick = () => {
+    const { idMeal } = meal;
+    const ingredients = [];
+    const MAX_LENGTH = 20;
+
+    for (let i = 0; i <= MAX_LENGTH; i += 1) {
+      const ingredient = meal[`strIngredient${i}`];
+      const measure = meal[`strMeasure${i}`];
+
+      if (ingredient && measure) {
+        ingredients.push(
+          `${measure} - ${ingredient}`,
+        );
+      }
+    }
+
+    // Passo 1: Recuperar o valor atual do localStorage
+    const inProgressRecipesJSON = localStorage.getItem('inProgressRecipes');
+    let inProgressRecipes = {};
+
+    // Verificar se já existe um valor no localStorage
+    if (inProgressRecipesJSON) {
+      inProgressRecipes = JSON.parse(inProgressRecipesJSON);
+    }
+
+    // Passo 2: Adicionar ou atualizar os dados desejados na estrutura do objeto
+    if (!inProgressRecipes.meals) {
+      inProgressRecipes.meals = {};
+    }
+
+    inProgressRecipes.meals[idMeal] = ingredients;
+
+    // Passo 3: Salvar o objeto atualizado de volta no localStorage
+    localStorage.setItem('inProgressRecipes', JSON.stringify(inProgressRecipes));
+  };
+
+  if (!meals) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <section className={ style.section }>
+      <h1 data-testid="recipe-title">{meal.strMeal}</h1>
+      <div className={ style.containerImg }>
+        <img
+          src={ meal.strMealThumb }
+          alt={ meal.strMeal }
+          data-testid="recipe-photo"
+          className={ style.imgRecipe }
+        />
+      </div>
+      <p
+        data-testid="recipe-category"
+        className={ style.category }
+      >
+        {meal.strCategory}
+      </p>
+      <h2>Ingredients:</h2>
+      <div className={ style.container }>
+        <ul>{renderIngredientsMeals()}</ul>
+      </div>
+      <h2>Instructions:</h2>
+      <div className={ style.container }>
+        <p data-testid="instructions">{meal.strInstructions}</p>
+      </div>
+      <h2>Video:</h2>
+      <div className={ style.container_video }>
+        <iframe title="Video Youtube" src={ meal.strYoutube } data-testid="video" />
+      </div>
+      <h2>Recommendation</h2>
+      {renderDrinksRecommendation()}
+      <button
+        data-testid="start-recipe-btn"
+        className={ style.btnStart }
+        onClick={ handleClick }
+      >
+        {isRecipeInProgress ? 'Continue Recipe' : 'Start Recipe'}
+      </button>
+    </section>
+  );
+}
+
+RenderMealsWithId.propTypes = {
+  drinksRecommendation: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+  loading: PropTypes.bool.isRequired,
+  meals: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+};
+
+export default RenderMealsWithId;
