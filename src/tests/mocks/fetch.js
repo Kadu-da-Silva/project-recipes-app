e343a769dc3fd2bf92d5fdd8b59b9b11ac45e4ea
Pedro de Andrade <pedrodeandrade2@gmail.com>
2023-06-15 14:44:37 -0300

    Finalizados os testes, comentado o mock

--- src/tests/mocks/fetch.js
+++ src/tests/mocks/fetch.js
@@ -1,19 +1,19 @@
-const mealsByIngredient = require('./mealsByIngredient');
+// const mealsByIngredient = require('./mealsByIngredient');
 
-const fetchMock = (url) => {
-  if (url === '') {
-    return Promise.resolve({
-      status: 200,
-      ok: true,
-      json: () => Promise.resolve(mealsByIngredient)
-    })
-  } else if (url === 'www.teste.com') {
-    return Promise.resolve({
-      status: 200,
-      ok: true,
-      json: () => Promise.resolve(mealsByIngredient)
-    })
-  }
-};
+// const fetchMock = (url) => {
+//   if (url === '') {
+//     return Promise.resolve({
+//       status: 200,
+//       ok: true,
+//       json: () => Promise.resolve(mealsByIngredient)
+//     })
+//   } else if (url === 'www.teste.com') {
+//     return Promise.resolve({
+//       status: 200,
+//       ok: true,
+//       json: () => Promise.resolve(mealsByIngredient)
+//     })
+//   }
+// };
 
-module.exports = fetchMock;
+// module.exports = fetchMock;
